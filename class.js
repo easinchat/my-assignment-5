140f8b38a97e7c9e094d48093517e67b6e9d67c0
easinchat <easin.chat@email.com>
2026-05-16 21:44:02 +0600

    change class.js

--- class.js
+++ class.js
@@ -12,3 +12,16 @@ class Player {
 
 const player1 = new Player("Yeasin", 33);
 console.log(player1);
+class Player {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.location = this.location;
+  }
+  goal() {
+    console.log("nothing to hide its confusing");
+  }
+}
+
+const player1 = new Player("arafat", 44);
+console.log(player1);
